c755bef7b3f3c05199dd8973b048afbb66accd0d
mohamed <mohamedabbj@gmail.com>
2024-05-23 20:38:01 -0400

    navbar first design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='outerNavbar'>
+      <div className='innerNavbar'>
+        <div className='content'>
+        <div className='logo'>
+        <img src="../src/assets/SVG.svg" alt="" /> <p>dogather</p>
+        </div>
+        <div className='pageButtons'>
+        <p>about us</p>
+        <img src="../src/assets/Vector.svg" alt="" />
+        <p>features</p>
+        <img src="../src/assets/Vector.svg" alt="" />
+        <p>contact</p>
+        <img src="../src/assets/Vector.svg" alt="" />
+        </div>
+        <div className='rightContent'>
+        <div className='login'>
+        <p>login</p>
+        <img src="../src/assets/Vector.svg" alt="" />
+        </div>
+        <button className='registerButton'>register</button>
+        </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
